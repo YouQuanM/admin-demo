95001a134a0906692e5ea8ac30e5c3af757df4ca
yinhang <yinhang@dankegongyu.com>
2019-03-26 16:35:11 +0800

    还原 api/data.js & user.js

---
+++ src/api/data.js
@@ -0,0 +1,37 @@
+import axios from '@/libs/axios-http'
+
+export const getLogList = () => {
+  return axios.get({
+    url: 'getList',
+    method: 'get'
+  })
+}
+
+export const getDragList = () => {
+  return axios.request({
+    url: 'get_drag_list',
+    method: 'get'
+  })
+}
+
+export const errorReq = () => {
+  return axios.request({
+    url: 'error_url',
+    method: 'post'
+  })
+}
+
+export const saveErrorLogger = info => {
+  return axios.request({
+    url: 'save_error_logger',
+    data: info,
+    method: 'post'
+  })
+}
+
+export const uploadImg = formData => {
+  return axios.request({
+    url: 'image/upload',
+    data: formData
+  })
+}
