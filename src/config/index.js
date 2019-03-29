487e6ffa213c84df94ac367b7b6e59ee1588abff
yinhang <yinhang@dankegongyu.com>
2019-03-29 17:52:13 +0800

    ip changed

--- src/config/index.js
+++ src/config/index.js
@@ -17,7 +17,7 @@ export default {
    * @description api请求基础路径
    */
   baseUrl: {
-    dev: 'http://172.16.41.111:9091/',
+    dev: 'http://172.16.31.86:8181/',
     pro: 'https://produce.com'
   },
   /**
